7f13a41b6aefe257b65c92b48741e3b407386552
Andrew Bell <fiixed@gmail.com>
2015-02-16 10:39:08 -0600

    initial commit

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    
+    
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func convertButton(sender: UIButton) {
+        let stringFromTextField = humanYearsTextField.text
+        var intFromString = stringFromTextField.toInt()!
+        var dogYears:Double
+        if intFromString <= 2 {
+             dogYears = Double(intFromString) * 10.5
+        } else {
+            dogYears = Double(intFromString) * 4.0 + 21
+        }
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(dogYears)"
+        
+    }
 }
 
